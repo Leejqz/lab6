03a2219284238a9c6bfda3b335cd2e3b6d319752
Leejqz <leessandra17@gmail.com>
2024-07-18 17:13:25 +0800

    initial

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex text-7xl min-h-screen flex-col items-center justify-between p-24">
+ Philip James Parreno, english or spanish of course filipino
+    </main>
+  );
+}
